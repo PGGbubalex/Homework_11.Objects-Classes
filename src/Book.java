e203486b729d37844e2127cc328e8c4bbce1a92f
Igor <99yearago99@gmail.com>
2022-10-27 20:35:59 +0300

    Fixed homework 12.

--- src/Book.java
+++ src/Book.java
@@ -4,20 +4,26 @@ public class Book {
     private Object Author;
     private int yearOfPublication;
 
-    public Book(String titleName, Object author, int yearOfPublication) {
+    public Book(String titleName, Author author, int yearOfPublication) {
 
         this.titleName = titleName;
         this.Author = author;
         this.yearOfPublication = yearOfPublication;
     }
     public String getTitleName() {
+
         return this.titleName;
     }
-    public Object getAuthor() {
+    public Author getAuthor() {
         return (Author) this.Author;
     }
     public int getYearOfPublication() {
+
         return this.yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         Author firstAuthor = new Author("Stephen", " King");
-        Book firstBook = new Book("It", firstAuthor, 1986);
+        Book firstBook = new Book("It", firstAuthor, 1985);
+            firstBook.setYearOfPublication(1986);
         Author secondAuthor = new Author("Dante", "Alighieri");
-        Book secondBook = new Book("The Divine Comedy", secondAuthor, 1320);
+        Book secondBook = new Book("The Divine Comedy", secondAuthor, 13200);
+            firstBook.setYearOfPublication(1320);
     }
 }
